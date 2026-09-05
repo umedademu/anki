1f5b2f88b5d7f48fbae72fc4a710e65dbadf22d4
umeda <umedademu@gmail.com>
2026-09-05 21:30:01 +0900

    ティムール遠征・死後編のドット絵における格子の境界ズレと混入ゴミを修正

--- public/safavid-story.js
+++ public/safavid-story.js
@@ -1,4 +1,4 @@
-import {places,zones,scenes} from "./safavid-scenes.js?v=0.186";
+import {places,zones,scenes} from "./safavid-scenes.js?v=0.187";
 
 const NS="http://www.w3.org/2000/svg";
 const project=([lon,lat])=>[(lon-20)*12,(58-lat)*15];
